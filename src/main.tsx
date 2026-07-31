187b466c1e37ed13fb9787e25809be23624183f0
niteshjangid-jiet <niteshjangid202@gmail.com>
2026-07-31 23:54:57 +0530

    Revert "feat: upgrade portfolio to PWA with custom logo icons and auto-update service worker"
    This reverts commit 1b10f0b99b4a54addc4c091736186a4fc2b31e9a.

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App'
 import { ThemeProvider } from './context/ThemeContext'
-import { setupSW } from './utils/registerSW'
-
-// Register PWA Service Worker for auto-update and offline capability
-setupSW();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,5 +11,4 @@ createRoot(document.getElementById('root')!).render(
     </ThemeProvider>
   </StrictMode>,
 )
-
 
